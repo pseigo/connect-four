c204ce21f2da8e214656cbc37dd317795661bb43
Peyton Seigo <pseigo@yahoo.com>
2019-05-02 13:41:32 -0600

    Change indents to 2 spaces (Google style); add moduledoc to Game.java

--- src/main/java/ca/peytonseigo/apps/listenerexample/MainTest.java
+++ src/main/java/ca/peytonseigo/apps/listenerexample/MainTest.java
@@ -4,14 +4,14 @@ package ca.peytonseigo.apps.listenerexample;
  * @author Peyton Seigo
  */
 public class MainTest {
-    public static void main(String[] args) {
-        ResponsibleAdult casandra = new ResponsibleAdult();
+  public static void main(String[] args) {
+    ResponsibleAdult casandra = new ResponsibleAdult();
 
-        Child jimmy = new Child("Jimmy", 11);
-        jimmy.addResponsibleAdult(casandra);
-        jimmy.makeTrouble();
-        jimmy.makeTrouble(); // this won't call propertyChange
-        jimmy.setAge(12);
-        jimmy.setAge(13);
-    }
+    Child jimmy = new Child("Jimmy", 11);
+    jimmy.addResponsibleAdult(casandra);
+    jimmy.makeTrouble();
+    jimmy.makeTrouble(); // this won't call propertyChange
+    jimmy.setAge(12);
+    jimmy.setAge(13);
+  }
 }
--- src/main/java/ca/peytonseigo/apps/model/Game.java
+++ src/main/java/ca/peytonseigo/apps/model/Game.java
@@ -1,24 +1,25 @@
 package ca.peytonseigo.apps.model;
 
 /**
+ * A Connect Four game that manages players, moderates game rules and tracks the state.
  * @author Peyton Seigo
  */
 public class Game {
-    /**
-     * Computes a thing. Here is some extra info that wouldn't fit into the one sentence description. You can
-     * reference names in the code using this syntax: {@code someParam}.
-     *
-     * @param someParam number of penguins
-     * @return short description of what is being returned
-     * @throws Exception if someParam is less than 5 (the acceptable threshold for penguins)
-     * @see <a href="https://www.tutorialspoint.com/java/java_documentation.htm">Documentation Comments</a> for further
-     * explanation and examples for writing Javadocs comments.
-     */
-    public int exampleMethod(int someParam) throws Exception {
-        if (someParam < 5) {
-            throw new Exception();
-        }
-        
-        return 0;
+  /**
+   * Computes a thing. Here is some extra info that wouldn't fit into the one sentence
+   * description. You can reference names in the code using this syntax: {@code someParam}.
+   *
+   * @param someParam number of penguins
+   * @return short description of what is being returned
+   * @throws Exception if someParam is less than 5 (the acceptable threshold for penguins)
+   * @see <a href="https://www.tutorialspoint.com/java/java_documentation.htm">Documentation
+   * Comments</a> for further explanation and examples for writing Javadocs comments.
+   */
+  public int exampleMethod(int someParam) throws Exception {
+    if (someParam < 5) {
+      throw new Exception();
     }
+
+    return 0;
+  }
 }
