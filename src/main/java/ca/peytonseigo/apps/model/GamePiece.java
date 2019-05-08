3d48ce653296c9295bc0cd8621e8d4d5a2cf582e
Peyton Seigo <pseigo@yahoo.com>
2019-05-08 16:13:26 -0600

    Create GamePiece enum for the board

---
+++ src/main/java/ca/peytonseigo/apps/model/GamePiece.java
@@ -0,0 +1,11 @@
+package ca.peytonseigo.apps.model;
+
+/**
+ * Represents a game piece on a Connect Four board.
+ * @author Peyton Seigo
+ */
+public enum GamePiece {
+  EMPTY,
+  PLAYER1,
+  PLAYER2
+}
