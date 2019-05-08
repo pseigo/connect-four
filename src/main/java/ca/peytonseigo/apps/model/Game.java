d2836d1232a4517fc69594b16b918b0280bfe909
Peyton Seigo <pseigo@yahoo.com>
2019-05-08 16:16:27 -0600

    Add TODOs, docs, and refactor methods in Game

--- src/main/java/ca/peytonseigo/apps/model/Game.java
+++ src/main/java/ca/peytonseigo/apps/model/Game.java
@@ -1,9 +1,10 @@
 package ca.peytonseigo.apps.model;
 
+import ca.peytonseigo.apps.exception.NotActivePlayerException;
 import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 
 import java.beans.PropertyChangeSupport;
-import java.util.List;
 
 /**
  * A Connect Four game that manages players, moderates game rules and tracks the state.
@@ -12,41 +13,47 @@ import java.util.List;
  * @author Madeline Ferguson
  */
 public class Game {
-  private Player turn;
-  private List<Player> players;
+  private Player activePlayer;
+  private BiMap<Player, GamePiece> playerGamePieces;
   private PropertyChangeSupport pcs;
 
-  public Game() {
+  /**
+   * Creates a new game between two given players. Adds each player as a listener for the
+   * {@code activePlayer} field.
+   * @param player1 one of the players
+   * @param player2 one of the players
+   */
+  public Game(Player player1, Player player2) {
+    pcs = new PropertyChangeSupport(this);
+    pcs.addPropertyChangeListener("activePlayer", player1);
+    pcs.addPropertyChangeListener("activePlayer", player2);
 
+    playerGamePieces = new ImmutableBiMap.Builder<Player, GamePiece>()
+        .put(player1, GamePiece.PLAYER1)
+        .put(player2, GamePiece.PLAYER2)
+        .build();
   }
 
   /**
-   * Gets who's turn it is.
+   * Returns true if it is currently the {@code player}'s turn, otherwise returns false.
    *
-   * @return who's turn it is
+   * @return true if {@code player} is the active player
    */
-  public Player getTurn() {
-    return turn;
+  public boolean isActive(Player player) {
+    return activePlayer.equals(player);
   }
 
+  // TODO: be wary of security (i.e., player1 might make commands on player2's behalf)
   /**
-   * Sets the turn. Fires property change event to notify players of turn change.
+   * Returns a reference to current player whose turn it is.
    *
-   * @param p The player who's turn it is being set to.
-   * @throws
+   * @return player whose turn it is
    */
-  public void setTurn(Player p) {
-    this.turn = p;
+  public Player activePlayer() {
+    return activePlayer;
   }
 
-  /**
-   * Changes the turn.
-   * For example, if the current value of turn is PLAYER1, changeTurn changes it to PLAYER2.
-   */
-  public void changeTurn() {
-
-  }
-
+  // TODO: might not need this, but we can keep it around until we get to testing
   /**
    * Adds a player to the game.
    *
@@ -56,29 +63,39 @@ public class Game {
 
   }
 
+  // TODO: throw `PlayerMoveException`s: NotActivePlayer, IllegalMove, InvalidBoardIndex, etc.
   /**
-   * Sends a move to the Board.
+   * Places a game piece for {@code player} at the given board {@code column} (0-based from the
+   * left). The player must pass a reference of itself for authorization purposes; the game needs to
+   * know who is making the move.
+   *
+   * @param player the player responsible for the move
    * @param column column to drop the piece into
+   * @throws NotActivePlayerException if it is not the {@code player}'s turn
    */
-  public void makeMove(int column){
-
+  public void makeMove(Player player, int column) throws NotActivePlayerException {
+    if (!isActive(player)) {
+      throw new NotActivePlayerException();
+    }
   }
 
   /**
-   * Computes a thing. Here is some extra info that wouldn't fit into the one sentence
-   * description. You can reference names in the code using this syntax: {@code someParam}.
-   *
-   * @param someParam number of penguins
-   * @return short description of what is being returned
-   * @throws Exception if someParam is less than 5 (the acceptable threshold for penguins)
-   * @see <a href="https://www.tutorialspoint.com/java/java_documentation.htm">Documentation
-   * Comments</a> for further explanation and examples for writing Javadocs comments.
+   * Advances the turn to the next player. Fires a {@code propertyChangeEvent} to notify any
+   * listeners (i.e., the players) of the turn change. In particular, if the current active player
+   * is player 1, @{code advanceTurn} changes it to player 2, and vice versa.
    */
-  public int exampleMethod(int someParam) throws Exception {
-    if (someParam < 5) {
-      throw new Exception();
-    }
+  private void advanceTurn() {
+    // ... setActivePlayer(...)
+    // notify listeners
+  }
 
-    return 0;
+
+  /**
+   * Sets the current active player (the player whose turn it is).
+   *
+   * @param p the new active player
+   */
+  private void setActivePlayer(Player p) {
+    this.activePlayer = p;
   }
 }
