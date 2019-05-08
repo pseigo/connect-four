545677da379c921dd467634add0f39e0c8902a80
Madeline Ferguson <lusul101@gamil.com>
2019-05-08 14:09:00 -0600

    Refactor turn in Game. Update documentation for setTurn() in Game.

--- src/main/java/ca/peytonseigo/apps/model/Player.java
+++ src/main/java/ca/peytonseigo/apps/model/Player.java
@@ -3,15 +3,11 @@ package ca.peytonseigo.apps.model;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import static ca.peytonseigo.apps.model.Turn.PLAYER1;
-import static ca.peytonseigo.apps.model.Turn.PLAYER2;
-
 /**
  * @author Peyton Seigo
  */
 public class Player implements PropertyChangeListener {
 
   public void propertyChange(PropertyChangeEvent event) {
-
   }
 }
