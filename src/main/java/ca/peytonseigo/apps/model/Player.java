635b39b7b2bcb0833a639a16feef5b400126d136
Madeline Ferguson <lusul101@gamil.com>
2019-05-08 14:14:58 -0600

    Add Player documentation.

--- src/main/java/ca/peytonseigo/apps/model/Player.java
+++ src/main/java/ca/peytonseigo/apps/model/Player.java
@@ -4,10 +4,16 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
+ * Player class.
  * @author Peyton Seigo
  */
 public class Player implements PropertyChangeListener {
-
+  /**
+   * This method gets called when a bound property is changed.
+   *
+   * @param event A PropertyChangeEvent object describing the event source and the property that
+   *     has changed.
+   */
   public void propertyChange(PropertyChangeEvent event) {
   }
 }
